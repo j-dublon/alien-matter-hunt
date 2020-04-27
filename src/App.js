a6384ae4432190745095982b02f815d8d3ace36a
j-dublon <j.johnson.dublon@gmail.com>
2020-04-27 17:10:34 +0100

    added functionality to fetch data from api and display all alien sites

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import React, { Component } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import AlienMap from "./Components/AlienMap";
+import axios from "axios";
 
 class App extends Component {
   state = {
-    meteoriteLandings: {},
-    selectedRegion: "Europe",
+    meteoriteLandings: [],
+    selectedRegion: "Africa",
+  };
+
+  componentDidMount = () => {
+    axios
+      .get("https://data.nasa.gov/resource/gh4g-9sfh.json?$limit=30")
+      .then((response) => {
+        this.setState((currentState) => {
+          return {
+            ...currentState.selectedRegion,
+            meteoriteLandings: response.data,
+          };
+        });
+      });
+  };
+
+  handleRegionSelect = (event) => {
+    const { value } = event.target;
+    this.setState((currentState) => {
+      return {
+        ...currentState.meteoriteLandings,
+        selectedRegion: value,
+      };
+    });
   };
 
   render() {
@@ -14,9 +38,12 @@ class App extends Component {
       <div className="App">
         <Header />
         <form>
-          <select id="cars" name="cars">
+          <select
+            id="regions"
+            name="regions"
+            onChange={this.handleRegionSelect}
+          >
             <option value="Africa">Africa</option>
-            <option value="Antartica">Antartica</option>
             <option value="Asia">Asia</option>
             <option value="Australasia">Australasia</option>
             <option value="Europe">Europe</option>
@@ -24,7 +51,10 @@ class App extends Component {
             <option value="SouthAmerica">South America</option>
           </select>
         </form>
-        <AlienMap selectedRegion={this.state.selectedRegion} />
+        <AlienMap
+          selectedRegion={this.state.selectedRegion}
+          meteorites={this.state.meteoriteLandings}
+        />
       </div>
     );
   }
--- src/Components/AlienMap.jsx
+++ src/Components/AlienMap.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-import { Icon } from "leaflet";
 
-function AlienMap({ selectedRegion }) {
+function AlienMap({ selectedRegion, meteorites }) {
   const regions = {
-    Africa: [11.405995, 21.635887, 4],
-    Antartica: [-84.813907, 22.737577, 4],
-    Asia: [49.787471, 91.654214, 4],
-    Australasia: [-18.542117, 138.2646, 4],
+    Africa: [4.227269, 21.418349, 3],
+    Asia: [49.787471, 91.654214, 2.5],
+    Australasia: [-18.542117, 138.2646, 3],
     Europe: [50.224367, 13.845184, 4],
-    NorthAmerica: [47.751328, -103.310138, 4],
-    SouthAmerica: [-19.369454, -58.006624, 4],
+    NorthAmerica: [47.751328, -103.310138, 2.5],
+    SouthAmerica: [-19.369454, -58.006624, 2.5],
   };
-  console.log(regions[selectedRegion][0]);
+
   return (
     <Map
       id="map"
       center={[regions[selectedRegion][0], regions[selectedRegion][1]]}
       zoom={regions[selectedRegion][2]}
     >
+      {meteorites.map((meteorite) => {
+        console.log(meteorite);
+        return (
+          <Marker
+            key={meteorite.id}
+            position={[meteorite.reclat, meteorite.reclong]}
+          />
+        );
+      })}
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
